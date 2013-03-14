7ba9113e202566f77e1414d81fb6cc2edebc1dda
Gurinder <gurinder_inder@yahoo.co.in>
2013-03-14 15:59:45 -0700

    made some test changes

--- src/main/java/com/guritech/Attachment.java
+++ src/main/java/com/guritech/Attachment.java
@@ -14,6 +14,7 @@ public class Attachment
      messageBodyPart.setDataHandler(new DataHandler(sources));
      messageBodyPart.setFileName(fileAttachments);
      multiparts.addBodyPart(messageBodyPart);
+}
 	
 
 
