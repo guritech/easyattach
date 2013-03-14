e9a390f5ee6f8d30d869a6ebd7b45fc6f2ad064e
Gurinder <gurinder_inder@yahoo.co.in>
2013-03-14 15:29:28 -0700

    initial back up

---
+++ src/main/java/com/guritech/Attachment.java
@@ -0,0 +1,19 @@
+package com.guritech;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.internet.MimeBodyPart;
+public class Attachment 
+{
+
+	int Multiparts ;
+	 String fileAttachment = "D:/Photos/4.jpg";
+	 multiparts.addBodyPart(messageBodyPart);
+     messageBodyPart  = new MimeBodyPart();
+     DataSource sources= new FileDataSource(fileAttachments);  // my file
+     messageBodyPart.setDataHandler(new DataHandler(sources));
+     messageBodyPart.setFileName(fileAttachments);
+     multiparts.addBodyPart(messageBodyPart);
+	
+
+
